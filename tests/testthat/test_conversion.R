042c8fb28dc22a0cd844a9f8d015974b6a38a4de
Thomas Mailund <mailund@birc.au.dk>
2016-08-13 09:40:30 +0200

    Do coverage test when committing to github using covr

--- tests/testthat/test_conversion.R
+++ tests/testthat/test_conversion.R
@@ -1,6 +1,6 @@
 context("Unit conversion")
 
-test_that("we can convert numbers to units", {
+test_that("we can convert numbers to unit-less units", {
   x <- as.units(1:4)
   expect_that(length(x), equals(4))
   expect_that(class(x), equals("units"))
@@ -8,5 +8,17 @@ test_that("we can convert numbers to units", {
   
   y <- 1:4
   units(y) <- "1"
+  expect_equal(x, y)
+})
+
+test_that("we can convert numbers to physical units", {
+  x <- as.units(1:4, "m")
+  expect_that(length(x), equals(4))
+  expect_that(class(x), equals("units"))
+  expect_that(units(x), equals("m"))
+  expect_equal(as.numeric(x), 1:4)
+  
+  y <- 1:4
+  units(y) <- "m"
   expect_equal(x, y)
 })
