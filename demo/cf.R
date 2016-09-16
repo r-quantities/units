1876d8b7ba4f5f164b62c5575771578b40018472
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-09-16 12:37:07 +0200

    add parse_unit test

--- demo/cf.R
+++ demo/cf.R
@@ -3,7 +3,12 @@ r = read_html(
  "http://cfconventions.org/Data/cf-standard-names/34/build/cf-standard-name-table.html")
 tb = html_table(r, fill = TRUE)
 library(units)
-u = lapply(tb[[3]][2][[1]], function(x) make_unit(x))
+cf_units = tb[[3]][2][[1]]
+u = lapply(cf_units, function(x) make_unit(x))
 names(u) = substr(tb[[3]][1][[1]], 1, 50)
 library(udunits2)
+# does udunits2 understand them?
 all(sapply(u, udunits2::ud.is.parseable))
+# can parse_unit parse them? remove "W m-2 sr-1 (m-1)-1"
+all(sapply(cf_units[cf_units != "W m-2 sr-1 (m-1)-1"],
+	function(x) class(parse_unit(x))) == "units")
