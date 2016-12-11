cc1724e2f0a8740a7b9a1c8398f52da8f78e41cb
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-12-11 21:07:01 +0100

    let unit compatibility check work for custom units

--- R/misc.R
+++ R/misc.R
@@ -6,10 +6,10 @@ c.units <- function(..., recursive = FALSE) {
     for (i in 2:length(args)) {
       if (!inherits(args[[i]], "units"))
         stop(paste("argument", i, "is not of class units"))
-      if (!ud.are.convertible(units(args[[i]]), u))
+      tr = try(units(args[[i]]) <- u)
+	  if (class(tr) == "try-error")
         stop(paste("argument", i, 
                    "has units that are not convertible to that of the first argument"))
-      units(args[[i]]) = u
     }
   x = unlist(args)
   as.units(x, u)
