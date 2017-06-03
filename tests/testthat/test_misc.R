e61b424d01f33e3621ee2697ac5991cf387fb570
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-06-03 19:03:46 +0200

    coverage

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -87,4 +87,9 @@ test_that("set_units works with character data, and resolves names", {
   Encoding(deg) = "UTF-8"
   expect_equal(set_units(1:2, deg), set_units(1:2, "degree_C"))
   expect_equal(set_units(1:2, deg), set_units(1:2, "degree_Celsius"))
+  # expect_error(set_units(1, TRUE), "is.character(value) is not TRUE")
+  x = set_units(1:3, km)
+  y <- set_units(x, "meter")
+  expect_equal(y, set_units(c(1000,2000,3000), m))
 })
+
