cb6dc1ebcdc412c22dcfaf82420f682f0bc10254
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-01-17 15:49:02 +0100

    header file

---
+++ src/units.h
@@ -0,0 +1,8 @@
+#include <udunits2.h>
+#include "Rcpp.h"
+
+using namespace Rcpp;
+
+typedef std::vector<void *> ut_vec;
+void finalizeUT(ut_vec *ptr);
+typedef XPtr<ut_vec,PreserveStorage,finalizeUT> XPtrUT;
