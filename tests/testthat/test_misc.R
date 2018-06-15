f2ee6fc846476e71df72715a82b25e953a8ce82f
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-06-15 15:47:12 +0200

    fix pillar test

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -35,7 +35,7 @@ test_that("We can use diff on a units object", {
 
 test_that("type_sum is available for units objects", {
   library(tibble)
-  expect_equal(type_sum(as_units("m")), "[m]")
+  expect_s3_class(type_sum(as_units("m")), "type_sum_units")
 })
 
 test_that("parse_unit works", {
