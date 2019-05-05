bcdf12e46dc272dd026fd58bebd78940b5bcff23
Iñaki Úcar <iucar@fedoraproject.org>
2019-05-05 21:47:11 +0200

    fix Celsius test, close #202

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -85,9 +85,7 @@ test_that("rep.units works", {
 })
 
 test_that("set_units works with symbols in character data, and resolves names", {
-  skip_on_os("windows") # encoding issue with degree:
-
-  deg = "°C"
+  deg = paste0(enc2native(intToUtf8(176)), "C")
   expect_equal(set_units(1:2, deg, mode = "standard"),        set_units(1:2, "degree_C", mode = "standard"))
   expect_equal(set_units(1:2, deg, mode = "standard"),        set_units(1:2, "degree_Celsius", mode = "standard"))
   expect_equal(set_units(1:2, "degree_C", mode = "standard"), set_units(1:2, "degree_Celsius", mode = "standard"))
