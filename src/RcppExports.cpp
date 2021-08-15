c77471a339fa2abb0fb91cf45aca8578dffe8eff
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2021-08-16 00:28:14 +0200

    tidy

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // udunits_exit
 void udunits_exit();
 RcppExport SEXP _units_udunits_exit() {
