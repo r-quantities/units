848dfa3b881fdbd3e82bb38a3a689c7446cc3868
olivroy <olivierroy71@hotmail.com>
2024-09-02 18:01:04 -0400

    Adopt new way of registering methods

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -78,37 +78,16 @@ vec_cast.units.units = function(x, to, ..., x_arg = "", to_arg = "") {
 
 #nocov start
 register_all_s3_methods <- function() {
-  register_s3_method("pillar::type_sum", "units")
-  register_s3_method("pillar::type_sum", "mixed_units")
-  register_s3_method("pillar::pillar_shaft", "units")
-  register_s3_method("pillar::pillar_shaft", "mixed_units")
-  register_s3_method("pillar::format_type_sum", "type_sum_units")
-  register_s3_method("vctrs::vec_proxy", "units")
-  register_s3_method("vctrs::vec_restore", "units")
-  register_s3_method("vctrs::vec_ptype2", "units.units")
-  register_s3_method("vctrs::vec_cast", "units.units")
-  register_s3_method("ggplot2::scale_type", "units")
-  register_s3_method("ggplot2::limits", "units")
-}
-
-register_s3_method <- function(generic, class, fun=NULL) {
-  stopifnot(is.character(generic), length(generic) == 1)
-  stopifnot(is.character(class), length(class) == 1)
-
-  pieces <- strsplit(generic, "::")[[1]]
-  stopifnot(length(pieces) == 2)
-  package <- pieces[[1]]
-  generic <- pieces[[2]]
-
-  if (is.null(fun))
-    fun <- get(paste0(generic, ".", class), envir=parent.frame())
-  stopifnot(is.function(fun))
-
-  if (package %in% loadedNamespaces())
-    registerS3method(generic, class, fun, envir=asNamespace(package))
-
-  # Always register hook in case package is later unloaded & reloaded
-  setHook(packageEvent(package, "onLoad"), function(...)
-    registerS3method(generic, class, fun, envir=asNamespace(package)))
+  s3_register("pillar::type_sum", "units")
+  s3_register("pillar::type_sum", "mixed_units")
+  s3_register("pillar::pillar_shaft", "units")
+  s3_register("pillar::pillar_shaft", "mixed_units")
+  s3_register("pillar::format_type_sum", "type_sum_units")
+  s3_register("vctrs::vec_proxy", "units")
+  s3_register("vctrs::vec_restore", "units")
+  s3_register("vctrs::vec_ptype2", "units.units")
+  s3_register("vctrs::vec_cast", "units.units")
+  s3_register("ggplot2::scale_type", "units")
+  s3_register("ggplot2::limits", "units")
 }
 # nocov end
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <!-- badges: start -->
 [![Build Status](https://github.com/r-quantities/units/workflows/build/badge.svg)](https://github.com/r-quantities/units/actions)
 [![Coverage Status](https://img.shields.io/codecov/c/github/r-quantities/units/master.svg)](https://app.codecov.io/github/r-quantities/units?branch=master)
-[![License](http://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html) [![CRAN](https://www.r-pkg.org/badges/version/units)](https://cran.r-project.org/package=units)
+[![License](https://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html) [![CRAN](https://www.r-pkg.org/badges/version/units)](https://cran.r-project.org/package=units)
 [![Downloads](https://cranlogs.r-pkg.org/badges/units?color=brightgreen)](https://www.r-pkg.org/pkg/units)
 <!-- badges: end -->
 
