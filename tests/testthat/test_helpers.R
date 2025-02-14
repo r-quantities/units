f15eac394679f555a516152896bbd87c5c339df4
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-14 02:26:40 -0800

    undo reformatting

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -1,10 +1,10 @@
-test_that("`keep_units()` restores units", {
+test_that("keep_units restores units", {
   x <- set_units(1:5, m)
 
   expect_identical(x, keep_units(drop_units, x))
 })
 
-test_that("`keep_units()` warns when no units are provided", {
+test_that("keep_units warns when no units are provided", {
   x <- 1:5
 
   expect_warning(keep_units(sum, x))
@@ -12,7 +12,7 @@ test_that("`keep_units()` warns when no units are provided", {
   expect_identical(suppressWarnings(keep_units(sum, x)), sum(x))
 })
 
-test_that("`keep_units()` sets units when `unit` argument is provide by user", {
+test_that("keep_units sets units when `unit` argument is provide by user", {
   rate <- set_units(3, "1/min")
   x <- keep_units(rexp, 3, rate, unit=units(1/rate))
   expect_identical(units(x), units(1/rate))
