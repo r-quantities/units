1263ec8bc247ccc6b941fa053230ab05848d80ca
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-14 02:12:06 -0800

    test 2 currently fails; have to rethink

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -1,13 +1,20 @@
-test_that("keep_units restores units", {
+test_that("`keep_units()` restores units", {
   x <- set_units(1:5, m)
 
   expect_identical(x, keep_units(drop_units, x))
 })
 
-test_that("keep_units warns when no units are provide", {
+test_that("`keep_units()` warns when no units are provided", {
   x <- 1:5
 
   expect_warning(keep_units(sum, x))
 
   expect_identical(suppressWarnings(keep_units(sum, x)), sum(x))
+})
+
+test_that("`keep_units()` sets units when `unit` argument is provide by user", {
+  rate <- set_units(3, "1/min")
+  x <- keep_units(rexp, 3, rate, unit=units(1/rate))
+  expect_identical(units(x), units(1/rate))
+
 })
